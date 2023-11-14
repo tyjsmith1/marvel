361405273fb4931ecf1249de922cb3681bd42fb1
tylersmith <tyjsmith1@gmail.com>
2023-11-14 10:21:10 -0600

    got started on JS

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 const url = "http://localhost:3000/characters"
 
+//hero variables
+const heroListContainer = document.getElementById("hero-list")
+
+
 
 //server test complete!
 fetch(url)
 .then(response => response.json())
-.then(data => console.log(data))
+.then(characters => {
+    const heroCharacters = characters.filter(character => character.class === "hero")
+    renderHeroList(characters)
+    console.log(heroCharacters)
+}
+)
+
+//1) populate hero list and villian list with character names only. 
+    // a) will need to filter the array by class so that heros and villians are separated
+
+function renderHeroList (characters) {
+    characters.forEach(character => {
+        const heroListItem = document.createElement("li")
+        heroListItem.textContent = character.name
+        heroListContainer.appendChild(heroListItem)
+    })
+}
+
